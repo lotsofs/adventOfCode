8dd7b2f024143766346b69144db61e933e101d1c
LotsOfS <18078548+lotsofs@users.noreply.github.com>
2025-12-02 01:54:12 +0100

    day1

---
+++ 2025/Program.cs
@@ -0,0 +1,36 @@
+﻿Puzzle1();
+
+static void Puzzle1() {
+    string[] pip01 = File.ReadAllLines("./01.pip");
+    int currentPosition = 50;
+    int password1 = 0;
+    int password2 = 0;
+    
+    DateTime start = DateTime.Now;
+    for (int i = 0; i < pip01.Length; i++) {
+        string p = pip01[i];
+        char direction = p[0];
+        int count = int.Parse(p[1..]);
+
+        for (int j = 0; j < count; j++) {
+            if (direction == 'L') {
+                currentPosition--;
+            }
+            else {
+                currentPosition++;
+            }
+            if (currentPosition % 100 == 0) {
+                password2++;
+                if (j == count - 1) {
+                    password1++;
+                }
+            }
+        }
+    }
+    DateTime end = DateTime.Now;
+    TimeSpan span = end - start;
+    double ms = span.TotalMilliseconds;
+    Console.WriteLine("password1: "+password1);
+    Console.WriteLine("password2: "+password2);
+    Console.WriteLine("Done, took "+ms+" ms");
+}
