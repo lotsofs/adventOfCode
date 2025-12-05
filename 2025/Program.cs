4642984d8a88429a10e3ad362d30ea2942fc6031
S <18078548+lotsofs@users.noreply.github.com>
2025-12-05 16:10:08 +0100

    d4

--- 2025/Program.cs
+++ 2025/Program.cs
@@ -2,200 +2,283 @@
 using System.Text.RegularExpressions;
 
 internal class Program {
-    private static readonly Stopwatch stopwatch = Stopwatch.StartNew();
-    private static long lastTick = 0;
+	private static readonly Stopwatch stopwatch = Stopwatch.StartNew();
+	private static long lastTick = 0;
 
-    private static void Main(string[] args) {
-        DumpTime("START");
-        if (Puzzle2("./02ex.pip") != 1227775554) {
-            throw new Exception("PUZZLE 2 EXAMPLE FAILED");
-        }
-        if (Puzzle3("./03ex.pip") != 3121910778619) {
-            throw new Exception("PUZZLE 3 EXAMPLE FAILED");
-        }
-        DumpTime("END OF EXAMPLES. START OF REAL PUZZLES");
-        Puzzle1();
-        Puzzle2();
-        Puzzle3();
+	private static void Main(string[] args) {
+		DumpTime("START");
+		if (Puzzle2("./02ex.pip") != 1227775554) {
+			throw new Exception("PUZZLE 2 EXAMPLE FAILED");
+		}
+		if (Puzzle3("./03ex.pip") != 3121910778619) {
+			throw new Exception("PUZZLE 3 EXAMPLE FAILED");
+		}
+		if (Puzzle4("./04ex.pip") != 43) {
+			throw new Exception("PUZZLE 4 EXAMPLE FAILED");
+		}
+		DumpTime("END OF EXAMPLES. START OF REAL PUZZLES");
+		Puzzle1();
+		Puzzle2();
+		Puzzle3();
+		Puzzle4();
 
-        static long Puzzle3(string fileName = "./03.pip") {
-            DumpTime("Puzzle 3 read start now");
-            string[] pip03 = File.ReadAllLines(fileName);
+		static long Puzzle4(string fileName = "./04.pip") {
+			DumpTime("P4 start");
+			string[] pip04 = File.ReadAllLines(fileName);
+			int rows = pip04[0].Length;
+			int cols = pip04.Length;
 
-            char x = '0';
-            int password1 = 0;
-            long password2 = 0;
+			int password1 = rows * cols;
+			int password2 = 0;
+			
+			bool[,] removedRolls = new bool[rows,cols];
+			int emergency = 1000;
+			bool firstPass = true;
+			int rollsRemovedThisPass;
 
-            DumpTime("Puzzle 3 calc start now");
-            foreach (string bank in pip03) {
-                int battery1 = x;
-                int battery2 = x;    
-                for (int i = 0; i < bank.Length; i++) {
-                    int battery = bank[i];
-                    if (i < bank.Length - 1 && battery > battery1) {
-                        battery1 = battery;
-                        battery2 = x;
-                    }
-                    else if (battery > battery2) {
-                        battery2 = battery;
-                    }
-                }
-                battery1 -= x;
-                battery1 *= 10;
-                battery2 -= x;
-                password1 += battery1 + battery2;
-            }
-            Console.WriteLine("password1: " + password1);
+			// Holy nesting :)
+			do {
+				rollsRemovedThisPass = 0;
+				emergency--;
+				if (emergency <= 0) {
+					break;
+				}
+				for (int row = 0; row < rows; row++) {
+					for (int col = 0; col < cols; col++) {
+						
+						if (pip04[row][col] == '.') {
+							password1--;
+							continue;
+						}
+						if (removedRolls[row,col]) {
+							continue;
+						}
+						int adjacentRolls = 0;
+						for (int r = row-1; r <= row+1; r++) {
+							for (int c = col-1; c <= col+1; c++) {
+								if (r < 0 || c < 0) {
+									continue;
+								}
+								if (r >= rows || c >= cols) {
+									continue;
+								}
+								if (r == row && c == col) {
+									continue;
+								}
+								bool isRoll = pip04[r][c] == '@';
+								bool isRemoved = removedRolls[r,c];
+								if (firstPass && isRoll) {
+									adjacentRolls++;
+									if (adjacentRolls == 4) {
+										password1--;
+									}
+								}
+								else if (isRoll && !isRemoved) {
+									adjacentRolls++;
+								}
+							}
+						}
+						if (adjacentRolls < 4) {
+							removedRolls[row,col] = true;
+							rollsRemovedThisPass++;
+						}
+					}
+				}
+				if (firstPass) {
+					Console.WriteLine("PW1=" + password1);
+					DumpTime("PW1 calced");
+					firstPass = false;
+				}
+				if (rollsRemovedThisPass == 0) {
+					break;
+				}
+				password2 += rollsRemovedThisPass;
+			}
+			while (rollsRemovedThisPass > 0);
 
-            DumpTime("Puzzle 3 start phase 2");
-            foreach (string bank in pip03) {
-                List<int> batterySequence = new List<int>();
-                for (int i = bank.Length - 12; i <= bank.Length - 1; i++) {
-                    batterySequence.Add(bank[i]-x);
-                }
+			Console.WriteLine("PW2=" + password2);
+			DumpTime("PW2 calced");
+			return password2;
+		}
 
-                char searchTarget = '9';
-                int minIndex = 0;
-                int maxIndex = bank.Length - 12;
-                int index = minIndex;
-                int emergency = 200*200*12;
-                int establishedBatteries = 0;
-                while (establishedBatteries < 12) {
-                    emergency--;
-                    if (emergency < 0) {
-                        break;
-                    }
-                    if (searchTarget < batterySequence[establishedBatteries]) {
-                        break;
-                    }
-                    int currentChar = bank[index] - x;
-                    if (currentChar == searchTarget) {
-                        batterySequence[establishedBatteries] = currentChar;
-                        establishedBatteries++;
-                        index++;
-                        minIndex = index;
-                        maxIndex++;
-                        continue;
-                    }    
-                    if (index == maxIndex) {
-                        searchTarget--;
-                        index = minIndex;
-                        continue;
-                    }
-                    index++;
-                }
-                long batSeq = 0;
-                for (int i = 0; i < batterySequence.Count; i++) {
-                    batSeq += batterySequence[i] * (long)Math.Pow(10, 11-i);
-                }
-                password2 += batSeq;
-            }
+		static long Puzzle3(string fileName = "./03.pip") {
+			DumpTime("Puzzle 3 read start now");
+			string[] pip03 = File.ReadAllLines(fileName);
 
-            DumpTime("day 3 end");
-            Console.WriteLine("password2: " + password2);
-            return password2;
-        }
+			char x = '0';
+			int password1 = 0;
+			long password2 = 0;
 
-        static long Puzzle2(string fileName = "./02.pip") {
-            DumpTime("Puzzle 2 start read");
-            string pip02 = File.ReadAllText(fileName);
+			DumpTime("Puzzle 3 calc start now");
+			foreach (string bank in pip03) {
+				int battery1 = x;
+				int battery2 = x;	
+				for (int i = 0; i < bank.Length; i++) {
+					int battery = bank[i];
+					if (i < bank.Length - 1 && battery > battery1) {
+						battery1 = battery;
+						battery2 = x;
+					}
+					else if (battery > battery2) {
+						battery2 = battery;
+					}
+				}
+				battery1 -= x;
+				battery1 *= 10;
+				battery2 -= x;
+				password1 += battery1 + battery2;
+			}
+			Console.WriteLine("password1: " + password1);
 
-            DumpTime("Puzzle 2 start split");
-            string[] ranges = pip02.Split(',');
-            long password1 = 0;
-            long password2 = 0;
+			DumpTime("Puzzle 3 start phase 2");
+			foreach (string bank in pip03) {
+				List<int> batterySequence = new List<int>();
+				for (int i = bank.Length - 12; i <= bank.Length - 1; i++) {
+					batterySequence.Add(bank[i]-x);
+				}
 
-            DumpTime("Puzzle 2 start process");
-            for (int i = 0; i < ranges.Length; i++) {
-                // This will not work if the range spans multiple factors of 100,
-                // Eg.: 1234-345678, but these are not present in the data. Sad :(
-                string[] bounds = ranges[i].Split('-');
-                long effectiveLowerBound = long.Parse(bounds[0]);
-                long actualLowerBound = effectiveLowerBound;
-                long upperBound = long.Parse(bounds[1]);
-                int digitCount = effectiveLowerBound.ToString().Length;
-                if (digitCount % 2 == 1) {
-                    effectiveLowerBound = (long)Math.Pow(10, digitCount);
-                    digitCount += 1;
-                }
+				char searchTarget = '9';
+				int minIndex = 0;
+				int maxIndex = bank.Length - 12;
+				int index = minIndex;
+				int emergency = 200*200*12;
+				int establishedBatteries = 0;
+				while (establishedBatteries < 12) {
+					emergency--;
+					if (emergency < 0) {
+						break;
+					}
+					if (searchTarget < batterySequence[establishedBatteries]) {
+						break;
+					}
+					int currentChar = bank[index] - x;
+					if (currentChar == searchTarget) {
+						batterySequence[establishedBatteries] = currentChar;
+						establishedBatteries++;
+						index++;
+						minIndex = index;
+						maxIndex++;
+						continue;
+					}	
+					if (index == maxIndex) {
+						searchTarget--;
+						index = minIndex;
+						continue;
+					}
+					index++;
+				}
+				long batSeq = 0;
+				for (int i = 0; i < batterySequence.Count; i++) {
+					batSeq += batterySequence[i] * (long)Math.Pow(10, 11-i);
+				}
+				password2 += batSeq;
+			}
 
-                long halfPow = (long)Math.Pow(10, digitCount / 2);
-                long halfDigits = effectiveLowerBound / halfPow;
+			DumpTime("day 3 end");
+			Console.WriteLine("password2: " + password2);
+			return password2;
+		}
 
-                for (long j = halfDigits; j < halfPow; j++) {
-                    long invalidNumber = j * halfPow + j;
-                    if (invalidNumber > upperBound)
-                    {
-                        break;
-                    }
-                    if (invalidNumber < actualLowerBound)
-                    {
-                        continue;
-                    }
-                    password1 += invalidNumber;
-                }
+		static long Puzzle2(string fileName = "./02.pip") {
+			DumpTime("Puzzle 2 start read");
+			string pip02 = File.ReadAllText(fileName);
 
-                // Strings D: Math much more fun
-                Regex r = new Regex(@"^(\w+)\1+$");
-                for (long k = actualLowerBound; k < upperBound; k++) {
-                    Match m = r.Match(k.ToString());
-                    if (m.Success) {
-                        password2 += long.Parse(m.Value);
-                    }
-                }
-            }
-            DumpTime("Puzzle 2 start print");
-            Console.WriteLine("password1: " + password1);
-            Console.WriteLine("password2: "+password2);
-            return password1;
-        }
+			DumpTime("Puzzle 2 start split");
+			string[] ranges = pip02.Split(',');
+			long password1 = 0;
+			long password2 = 0;
 
-        static void Puzzle1() {
-            DumpTime("Start of puzzle 1 read");
-            string[] pip01 = File.ReadAllLines("./01.pip");
-            int currentPosition = 50;
-            int password1 = 0;
-            int password2 = 0;
+			DumpTime("Puzzle 2 start process");
+			for (int i = 0; i < ranges.Length; i++) {
+				// This will not work if the range spans multiple factors of 100,
+				// Eg.: 1234-345678, but these are not present in the data. Sad :(
+				string[] bounds = ranges[i].Split('-');
+				long effectiveLowerBound = long.Parse(bounds[0]);
+				long actualLowerBound = effectiveLowerBound;
+				long upperBound = long.Parse(bounds[1]);
+				int digitCount = effectiveLowerBound.ToString().Length;
+				if (digitCount % 2 == 1) {
+					effectiveLowerBound = (long)Math.Pow(10, digitCount);
+					digitCount += 1;
+				}
 
-            DumpTime("Start of puzzle 1 process");
-            // Dumbly do the rotations. Tried doing something fancy
-            // but having to deal with all the edge cases just made it
-            // ugly but with no significant bonus satisfaction.
-            for (int i = 0; i < pip01.Length; i++) {
-                string p = pip01[i];
-                char direction = p[0];
-                int count = int.Parse(p[1..]);
+				long halfPow = (long)Math.Pow(10, digitCount / 2);
+				long halfDigits = effectiveLowerBound / halfPow;
 
-                for (int j = 0; j < count; j++)
-                {
-                    if (direction == 'L') {
-                        currentPosition--;
-                    }
-                    else {
-                        currentPosition++;
-                    }
-                    if (currentPosition % 100 == 0) {
-                        password2++;
-                        if (j == count - 1) {
-                            password1++;
-                        }
-                    }
-                }
-            }
-            DumpTime("End of puzzle 1");
-            Console.WriteLine("password1: " + password1);
-            Console.WriteLine("password2: " + password2);
-        }
-        
-        static void DumpTime(string log = "") {
-            long current = stopwatch.ElapsedTicks;
-            double ticksPerMs = Stopwatch.Frequency / 1000;
-            double totalMs = (double)current / ticksPerMs;
-            double deltaMs = lastTick == 0 ? totalMs : (double)(current-lastTick) / ticksPerMs;
-            Console.WriteLine("");
-            Console.WriteLine($"Elapsed time since start: {totalMs:0.###} ms, since last dump: {deltaMs:0.###} ms");
-            Console.WriteLine($"========{log}========");
-            lastTick = current;
-        }
-    }
+				for (long j = halfDigits; j < halfPow; j++) {
+					long invalidNumber = j * halfPow + j;
+					if (invalidNumber > upperBound)
+					{
+						break;
+					}
+					if (invalidNumber < actualLowerBound)
+					{
+						continue;
+					}
+					password1 += invalidNumber;
+				}
+
+				// Strings D: Math much more fun
+				Regex r = new Regex(@"^(\w+)\1+$");
+				for (long k = actualLowerBound; k < upperBound; k++) {
+					Match m = r.Match(k.ToString());
+					if (m.Success) {
+						password2 += long.Parse(m.Value);
+					}
+				}
+			}
+			DumpTime("Puzzle 2 start print");
+			Console.WriteLine("password1: " + password1);
+			Console.WriteLine("password2: "+password2);
+			return password1;
+		}
+
+		static void Puzzle1() {
+			DumpTime("Start of puzzle 1 read");
+			string[] pip01 = File.ReadAllLines("./01.pip");
+			int currentPosition = 50;
+			int password1 = 0;
+			int password2 = 0;
+
+			DumpTime("Start of puzzle 1 process");
+			// Dumbly do the rotations. Tried doing something fancy
+			// but having to deal with all the edge cases just made it
+			// ugly but with no significant bonus satisfaction.
+			for (int i = 0; i < pip01.Length; i++) {
+				string p = pip01[i];
+				char direction = p[0];
+				int count = int.Parse(p[1..]);
+
+				for (int j = 0; j < count; j++)
+				{
+					if (direction == 'L') {
+						currentPosition--;
+					}
+					else {
+						currentPosition++;
+					}
+					if (currentPosition % 100 == 0) {
+						password2++;
+						if (j == count - 1) {
+							password1++;
+						}
+					}
+				}
+			}
+			DumpTime("End of puzzle 1");
+			Console.WriteLine("password1: " + password1);
+			Console.WriteLine("password2: " + password2);
+		}
+		
+		static void DumpTime(string log = "") {
+			long current = stopwatch.ElapsedTicks;
+			double ticksPerMs = Stopwatch.Frequency / 1000;
+			double totalMs = (double)current / ticksPerMs;
+			double deltaMs = lastTick == 0 ? totalMs : (double)(current-lastTick) / ticksPerMs;
+			Console.WriteLine("");
+			Console.WriteLine($"Elapsed time since start: {totalMs:0.###} ms, since last dump: {deltaMs:0.###} ms");
+			Console.WriteLine($"========{log}========");
+			lastTick = current;
+		}
+	}
 }
